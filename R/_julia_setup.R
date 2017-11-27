fb5177a31e7c1452b7084f3d96e6f1724a8e4165
kahaaga <kahaaga@gmail.com>
2017-11-27 22:58:18 +0100

    Install SimplexIntersection package.

--- R/_julia_setup.R
+++ R/_julia_setup.R
@@ -4,6 +4,17 @@
 #'
 #' @export
 setup_julia <- function() {
+  # Set up Julia
   JuliaCall::julia_setup()
+
+  # Check if SimplexIntersection Julia package exists.
+  pkg_exists <- JuliaCall::julia_exists("SimplexIntersection")
+
+  # If it doesn't, install it.
+  if (!pkg_exists) {
+    julia$call("Pkg.clone", "git@github.com:kahaaga/SimplexIntersection.jl.git")
+  }
+
+  # Load the SimplexIntersection package
   JuliaCall::julia_library("SimplexIntersection")
 }
