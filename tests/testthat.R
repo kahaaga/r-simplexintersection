922fa6176a35538d62af79dcd1b702bd68cfd6d2
kahaaga <kahaaga@gmail.com>
2017-11-28 04:02:07 +0100

    Load JuliaCall.

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,8 +2,8 @@ Sys.setenv("R_TESTS" = "")
 
 ## Need to do julia_setup() before loading the testthat library.
 ## dyn.load libjulia after library(testthat) is problematic.
+library(JuliaCall)
 julia_setup()
 library(testthat)
 
-test_check("JuliaCall")
 test_check("rSimplexIntersection")
