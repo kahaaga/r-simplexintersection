2ca739150befcc54c8fafbdd76a38993d2fc64b7
kahaaga <kahaaga@gmail.com>
2017-11-28 03:08:49 +0100

    Don't import package.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,9 @@
+Sys.setenv("R_TESTS" = "")
+
+## Need to do julia_setup() before loading the testthat library.
+## dyn.load libjulia after library(testthat) is problematic.
+julia_setup()
 library(testthat)
-library(rSimplexIntersection)
 
-
+test_check("JuliaCall")
 test_check("rSimplexIntersection")
